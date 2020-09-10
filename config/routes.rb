0c31c95ae53e43042c59eca40e3ca3804a533ec0
gn2481 <gn2481@gmail.com>
2020-09-10 11:18:38 +0800

    add_admin_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 指定根目錄為貼文首頁
+  root 'post#index'
+
+  resources :admin do
+    collection do
+      get :sing_in # user/sign_in
+      post :login
+      post :edit
+      patch :update
+    end
+  end
+
+  # post
+  resources :post
+  # profile
+  # resources :profile
 end
